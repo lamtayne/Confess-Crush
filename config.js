5f2a07a5a5edec37663af7afe891e7d84433f809
lamtayne <106748842+lamtayne@users.noreply.github.com>
2022-06-02 19:58:25 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    titleWeb: "Gửi con chó",
+    introTitle: 'Bùi Mai Linh',
+    introDesc: `Puoi Mai Linh là ĐBRR`,
+    btnIntro: '^^concac^^',
+    title: 'Con lon Mai Linh là chó 🥰',
+    desc: 'Vẫn vậy nhưng mà dòng dưới ',
+    btnYes: 'chuẩn k cần chỉnh',
+    btnNo: 'đéo',
+    question: 'Linh là chó linh là súc vật',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    reply: 'Đúng thì đúng mà k đúng thì cũng đúng',
+    mess: 'Mình biết mà 🥰. Cửm ưn bạn nhiều nhiều 😘😘',
+    messDesc: 'Thế chốt lại mày làm chó nhá.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+    messLink: 'https://www.facebook.com/profile.php?id=100024199633772' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
